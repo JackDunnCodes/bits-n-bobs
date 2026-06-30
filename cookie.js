3bd83027027b84e5c2df90dd6356f7b08b217602
Jack Dunn <56315967+JackDunnCodes@users.noreply.github.com>
2026-06-30 13:59:52 +0100

    cookie.js smaller!!!

--- cookie.js
+++ cookie.js
@@ -1,11 +1,12 @@
 /**
- * Initialise a cookie object. Minifies to less than 490 bytes!
+ * Initialise a cookie object. Minifies to less than 410 bytes (312 bytes gzipped)!
+ * Note: Don't include as a seperate file and create a bunch of overhead. Just copy-paste into your own JS.
  * @param {string} name 
  * @author Jack Dunn <https://github.com/JackDunnCodes>
  * @license MPL-2.0
  */
 function cookie(name) {
-  let _docCookie = (value) => {return document.cookie=value}
+  let _docCookie = (value) => {return document.cookie=value}, _eqChar = '=';
     /**
      * Get the value of this cookie
      * @returns The cookie value
@@ -14,26 +15,27 @@ function cookie(name) {
         let cookieString = decodeURIComponent(document.cookie);
         let cookies = cookieString.split(';');
         for(let i in cookies) {
-            let cookie = cookies[i].trim().split('=');
+            let cookie = cookies[i].trim().split(_eqChar);
             let cookieName = cookie[0];
             if (cookieName === name) {
-                return cookie.slice(1).join('=');
+                return cookie.slice(1).join(_eqChar);
             }
         }
         return undefined;
-    }
+    };
     /**
      * Set the value of the cookie, with an optional timeout.
      * @param {*} value 
      * @param {number} seconds 
      */
-    let set = (value, seconds = null) => {
-        const nvp =name + "=" + value+ ";path=/";
-        if(seconds === null) {
-            _docCookie(nvp);
-        } else {
-            _docCookie(nvp + ";expires=" + (new Date(Date.now()+ (seconds*1000))).toUTCString());
-        }
-    }
+    let set = (value, seconds = NaN) => 
+        _docCookie(
+            name + _eqChar + value + ";path=/"
+            + (
+                isNaN(seconds) ? '' : (
+                    ";expires=" + (new Date(Date.now() + (seconds*1000))).toUTCString()
+                )
+            )
+        );
     return {value, set, delete: ()=>{return set("",0)}, toString:value};
 }
