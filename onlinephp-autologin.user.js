6afefcb1de66ee4aa34324c32c4038aa51e1dc46
Jack Dunn <56315967+JackDunnCodes@users.noreply.github.com>
2026-07-06 13:15:21 +0100

    Update version and refactor overlay function
    Signed-off-by: Jack Dunn <56315967+JackDunnCodes@users.noreply.github.com>

--- onlinephp-autologin.user.js
+++ onlinephp-autologin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OnlinePHP.io - auto-login via GitHub
 // @namespace    http://github.com/jackdunncodes/
-// @version      2026-07-02.01
+// @version      2026-07-06.01
 // @description  Premium account keep logging you out? This is for you.
 // @author       Jack Dunn
 // @match        https://onlinephp.io
@@ -18,35 +18,29 @@
 (function() {
     'use strict';
 
+    function overlayIt() {
+        const overlay = document.createElement('div');
+                overlay.style.cssText = `
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.5);
+    z-index: 9999;
+`;
+        document.body.appendChild(overlay);
+    }
+    
     function go() {
         try{
             if(document.querySelector('button.login-github') !== null) {
                 window.stop();
-                const overlay = document.createElement('div');
-                overlay.style.cssText = `
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,0.5);
-    z-index: 9999;
-`;
-                document.body.appendChild(overlay);
+                overlayIt();
 
                 document.querySelector('button.login-github').click();
             } else if (document.querySelector('a[href="https://onlinephp.io/login"]') !== null) {
-                const overlay = document.createElement('div');
-                overlay.style.cssText = `
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,0.5);
-    z-index: 9999;
-`;
-                document.body.appendChild(overlay);
+                overlayIt();
 
                 document.querySelector('a[href="https://onlinephp.io/login"]').click();
                 window.setTimeout(go, 1000);
